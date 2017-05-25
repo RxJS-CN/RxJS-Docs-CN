67a172575aaeb875939d523ce48865c0bea46074
jianlei.hjl <jianlei.hjl@alibaba-inc.com>
2017-05-25 13:44:40 +0800

    feat:add defer

--- src/observable/DeferObservable.ts
+++ src/observable/DeferObservable.ts
@@ -18,12 +18,12 @@ export class DeferObservable<T> extends Observable<T> {
    * </span>
    *
    * <img src="./img/defer.png" width="100%">
-   * `defer` 允许你创建一个Observable当且仅当它被订阅的时候，并且会每个订阅者创建新的Observable.
+   * `defer`允许你创建一个Observable当且仅当它被订阅的时候，并且为每个订阅者创建新的Observable.
    * 它一直在等待直到观察者订阅了它, 然后它创建一个新的Observable,通常会以Observable工厂函数的方式.
    * 对每个订阅者它都是新的, 所以即使每个订阅者也许会认为它们订阅的是同一个Observable, 事实上每个订阅
-   * 者获得的只属于它们的Observable.
+   * 者获得的是只属于它们的Observable.
    *
-   * @example <caption>Subscribe to either an Observable of clicks or an Observable of interval, at random</caption>
+   * @example <caption>随机订阅点击或者间隔Observable</caption>
    * var clicksOrInterval = Rx.Observable.defer(function () {
    *   if (Math.random() > 0.5) {
    *     return Rx.Observable.fromEvent(document, 'click');
@@ -34,19 +34,16 @@ export class DeferObservable<T> extends Observable<T> {
    * clicksOrInterval.subscribe(x => console.log(x));
    *
    * // Results in the following behavior:
-   * // If the result of Math.random() is greater than 0.5 it will listen
-   * // for clicks anywhere on the "document"; when document is clicked it
-   * // will log a MouseEvent object to the console. If the result is less
-   * // than 0.5 it will emit ascending numbers, one every second(1000ms).
+   * // 如果Math.random()返回的值大于0.5，它会监听"document"上的点击事件; 当document
+   * // 被电击，它会将电击事件对象打印到控制台. 如果结果小于0.5它会每秒发出一个从0开始自增数.
    *
    * @see {@link create}
    *
-   * @param {function(): SubscribableOrPromise} observableFactory The Observable
-   * factory function to invoke for each Observer that subscribes to the output
-   * Observable. May also return a Promise, which will be converted on the fly
-   * to an Observable.
-   * @return {Observable} An Observable whose Observers' subscriptions trigger
-   * an invocation of the given Observable factory function.
+   * @param {function(): SubscribableOrPromise} observableFactory Observable的工
+   * 厂函数会在每个Observer订阅Observable的时候被触发调用. 也许会返回一个Promise, 将会被转
+   * 化为Observable.
+   * @return {Observable} 一个Observable，它的Observer订阅的时候会触发对Observable工厂
+   * 函数的调用.
    * @static true
    * @name defer
    * @owner Observable
