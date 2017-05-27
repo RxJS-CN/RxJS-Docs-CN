ea30dde894f417e0f32ccc75026c861a32b9b89d
SangKa.Z <sangka23@163.com>
2017-05-27 16:56:58 +0800

    docs(mapTo): translate api mapTo.ts<100%>

--- src/operator/mapTo.ts
+++ src/operator/mapTo.ts
@@ -3,28 +3,25 @@ import { Subscriber } from '../Subscriber';
 import { Observable } from '../Observable';
 
 /**
- * Emits the given constant value on the output Observable every time the source
- * Observable emits a value.
+ * 每次源 Observble 发出值时，都在输出 Observable 上发出给定的常量值。
  *
- * <span class="informal">Like {@link map}, but it maps every source value to
- * the same output value every time.</span>
+ * <span class="informal">类似于 {@link map}，但它每一次都把源值映射成同一个输出值。</span>
  *
  * <img src="./img/mapTo.png" width="100%">
  *
- * Takes a constant `value` as argument, and emits that whenever the source
- * Observable emits a value. In other words, ignores the actual source value,
- * and simply uses the emission moment to know when to emit the given `value`.
+ * 接收常量 `value` 作为参数，并每当源 Observable 发出值时都发出这个值。换句话说，
+ * 就是忽略实际的源值，然后简单地使用这个发送时间点以知道何时发出给定的 `value` 。
  *
- * @example <caption>Map every click to the string 'Hi'</caption>
+ * @example <caption>把每次点击映射成字符串 'Hi'</caption>
  * var clicks = Rx.Observable.fromEvent(document, 'click');
  * var greetings = clicks.mapTo('Hi');
  * greetings.subscribe(x => console.log(x));
  *
  * @see {@link map}
  *
- * @param {any} value The value to map each source value to.
- * @return {Observable} An Observable that emits the given `value` every time
- * the source Observable emits something.
+ * @param {any} value 将每个源值映射成的值。
+ * @return {Observable} 该 Observable 在每次源 Observable 发出值的时候发出给定
+ * 的 `value` 。
  * @method mapTo
  * @owner Observable
  */
