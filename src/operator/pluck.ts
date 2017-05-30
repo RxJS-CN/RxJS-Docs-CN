16e4f6ded6f00ce1d7e2bed7c413174909ee4ded
SangKa.Z <sangka23@163.com>
2017-05-30 21:01:05 +0800

    docs(pluck): translate api pluck.ts<100%>

--- src/operator/pluck.ts
+++ src/operator/pluck.ts
@@ -2,28 +2,24 @@ import { Observable } from '../Observable';
 import { map } from './map';
 
 /**
- * Maps each source value (an object) to its specified nested property.
+ * 将每个源值(对象)映射成它指定的嵌套属性。
  *
- * <span class="informal">Like {@link map}, but meant only for picking one of
- * the nested properties of every emitted object.</span>
+ * <span class="informal">类似于 {@link map}，但仅用于选择每个发出对象的某个嵌套属性。</span>
  *
  * <img src="./img/pluck.png" width="100%">
  *
- * Given a list of strings describing a path to an object property, retrieves
- * the value of a specified nested property from all values in the source
- * Observable. If a property can't be resolved, it will return `undefined` for
- * that value.
+ * 给定描述对象属性路径的字符串的列表，然后从源 Observable 中的所有值中检索指定嵌套
+ * 属性的值。如果属性无法解析，它会返回 `undefined` 。
  *
- * @example <caption>Map every every click to the tagName of the clicked target element</caption>
+ * @example <caption>将每次点击映射成点击的 target 元素的 tagName 属性</caption>
  * var clicks = Rx.Observable.fromEvent(document, 'click');
  * var tagNames = clicks.pluck('target', 'tagName');
  * tagNames.subscribe(x => console.log(x));
  *
  * @see {@link map}
  *
- * @param {...string} properties The nested properties to pluck from each source
- * value (an object).
- * @return {Observable} A new Observable of property values from the source values.
+ * @param {...string} properties 从每个源值(对象啊)中提取的嵌套属性。
+ * @return {Observable} 全新的 Observable，它发出源自源值的属性值。
  * @method pluck
  * @owner Observable
  */
