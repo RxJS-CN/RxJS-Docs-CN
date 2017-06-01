13d547a1aaac511089de95ff4878cec8dde2182d
SangKa.Z <sangka23@163.com>
2017-06-01 15:04:08 +0800

    docs(operators): polish translation of operators
    operator list:
    
    - dematerialize
    - distinct
    - distinctUntilChanged
    - distinctUntilKeyChanged
    - materialize

--- src/operator/distinct.ts
+++ src/operator/distinct.ts
@@ -8,10 +8,10 @@ import { subscribeToResult } from '../util/subscribeToResult';
 import { ISet, Set } from '../util/Set';
 
 /**
- * 返回 Observable，它发出源 Observable 发出的所有与之前所有项都不相同的项。
+ * 返回 Observable，它发出由源 Observable 所发出的所有与之前所有项都不相同的项。
  * 
- * 如果提供了 keySelector 函数，那么它会将源 Observable 的每个值都投射成一个新的值，这个值会用来检查是否与前一个投射值相等。如果没有提供 
- * keySelector 函数，它会直接使用源 Observable 的每个值来检查是否与前一个值相等。
+ * 如果提供了 keySelector 函数，那么它会将源 Observable 的每个值都投射成一个新的值，这个值会用来检查是否与先前投射的值相等。如果没有提供 
+ * keySelector 函数，它会直接使用源 Observable 的每个值来检查是否与先前的值相等。
  *
  * 在支持 `Set` 的 JavaScript 运行时中，此操作符会使用 `Set` 来提升不同值检查的性能。
  *
@@ -46,7 +46,7 @@ import { ISet, Set } from '../util/Set';
  *
  * @param {function} [keySelector] 可选函数，用来选择要检查哪个值是不同的。
  * @param {Observable} [flushes] 可选 Observable，用来清空操作符内部的 HashSet 。
- * @return {Observable} Observable 发出从源 Observable 中得到的不同的值。
+ * @return {Observable} 该 Observable 发出从源 Observable 中得到的不同的值。
  * @method distinct
  * @owner Observable
  */
