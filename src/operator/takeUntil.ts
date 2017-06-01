b8bcf241cc06d057828f5deb6a73bf9c6a4ae3fb
SangKa.Z <sangka23@163.com>
2017-06-01 11:25:10 +0800

    docs(takeUntil): translate api takeUntil.ts<100%>

--- src/operator/takeUntil.ts
+++ src/operator/takeUntil.ts
@@ -8,20 +8,18 @@ import { InnerSubscriber } from '../InnerSubscriber';
 import { subscribeToResult } from '../util/subscribeToResult';
 
 /**
- * Emits the values emitted by the source Observable until a `notifier`
- * Observable emits a value.
+ * 发出源 Observable 发出的值，直到 `notifier` Observable 发出值。
  *
- * <span class="informal">Lets values pass until a second Observable,
- * `notifier`, emits something. Then, it completes.</span>
+ * <span class="informal">让值通过直到第二个 Observable ，
+ * 即 `notifier` 发出东西。然后它便完成。</span>
  *
  * <img src="./img/takeUntil.png" width="100%">
  *
- * `takeUntil` subscribes and begins mirroring the source Observable. It also
- * monitors a second Observable, `notifier` that you provide. If the `notifier`
- * emits a value or a complete notification, the output Observable stops
- * mirroring the source Observable and completes.
+ * `takeUntil` 订阅并开始镜像源 Observable 。它还监视另外一个 Observable，即你
+ * 提供的 `notifier` 。如果 `notifier` 发出值或 `complete` 通知，那么输出 Observable 
+ * 停止镜像源 Observable ，然后完成。
  *
- * @example <caption>Tick every second until the first click happens</caption>
+ * @example <caption>每秒都发出值，直到第一次点击发生</caption>
  * var interval = Rx.Observable.interval(1000);
  * var clicks = Rx.Observable.fromEvent(document, 'click');
  * var result = interval.takeUntil(clicks);
@@ -32,11 +30,10 @@ import { subscribeToResult } from '../util/subscribeToResult';
  * @see {@link takeWhile}
  * @see {@link skip}
  *
- * @param {Observable} notifier The Observable whose first emitted value will
- * cause the output Observable of `takeUntil` to stop emitting values from the
- * source Observable.
- * @return {Observable<T>} An Observable that emits the values from the source
- * Observable until such time as `notifier` emits its first value.
+ * @param {Observable} notifier 该 Observable 第一次发出值会使 `takeUntil` 的
+ * 输出 Observable 停止发出由源 Observable 所发出的值。
+ * @return {Observable<T>} 该 Observable 发出源 Observable 所发出的值，直到某个
+ * 时间点 `notifier` 发出它的第一个值。
  * @method takeUntil
  * @owner Observable
  */
