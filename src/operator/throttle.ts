4fee6340aee767943f3a8d59a675449f083750e7
SangKa.Z <sangka23@163.com>
2017-06-03 09:46:47 +0800

    docs(throttle): translate api throttle.ts<100%>

--- src/operator/throttle.ts
+++ src/operator/throttle.ts
@@ -18,25 +18,21 @@ export const defaultThrottleConfig: ThrottleConfig = {
 };
 
 /**
- * Emits a value from the source Observable, then ignores subsequent source
- * values for a duration determined by another Observable, then repeats this
- * process.
+ * 从源 Observable 中发出一个值，然后在由另一个 Observable 决定的期间内忽略
+ * 随后发出的源值，然后重复此过程。
  *
- * <span class="informal">It's like {@link throttleTime}, but the silencing
- * duration is determined by a second Observable.</span>
+ * <span class="informal">它很像 {@link throttleTime}，但是沉默持续时间是由
+ * 第二个 Observable 决定的。</span>
  *
  * <img src="./img/throttle.png" width="100%">
  *
- * `throttle` emits the source Observable values on the output Observable
- * when its internal timer is disabled, and ignores source values when the timer
- * is enabled. Initially, the timer is disabled. As soon as the first source
- * value arrives, it is forwarded to the output Observable, and then the timer
- * is enabled by calling the `durationSelector` function with the source value,
- * which returns the "duration" Observable. When the duration Observable emits a
- * value or completes, the timer is disabled, and this process repeats for the
- * next source value.
+ * 当 `throttle` 的内部定时器禁用时，它会在输出 Observable 上发出源 Observable 的值，
+ * 并当定时器启用时忽略源值。最开始时，定时器是禁用的。一旦第一个源值达到，它会被转发
+ * 到输出 Observable ，然后通过使用源值调用 `durationSelector` 函数来启动定时器，这
+ * 个函数返回 "duration" Observable 。当 duration Observable 发出值或完成时，定时器
+ * 会被禁用，并且下一个源值也是重复此过程。
  *
- * @example <caption>Emit clicks at a rate of at most one click per second</caption>
+ * @example <caption>以每秒最多点击一次的频率发出点击事件</caption>
  * var clicks = Rx.Observable.fromEvent(document, 'click');
  * var result = clicks.throttle(ev => Rx.Observable.interval(1000));
  * result.subscribe(x => console.log(x));
@@ -47,13 +43,12 @@ export const defaultThrottleConfig: ThrottleConfig = {
  * @see {@link sample}
  * @see {@link throttleTime}
  *
- * @param {function(value: T): SubscribableOrPromise} durationSelector A function
- * that receives a value from the source Observable, for computing the silencing
- * duration for each source value, returned as an Observable or a Promise.
- * @param {Object} config a configuration object to define `leading` and `trailing` behavior. Defaults
- * to `{ leading: true, trailing: false }`.
- * @return {Observable<T>} An Observable that performs the throttle operation to
- * limit the rate of emissions from the source.
+ * @param {function(value: T): SubscribableOrPromise} durationSelector 该函数
+ * 从源 Observable 中接收值，用于为每个源值计算沉默持续时间，并返回 Observable 或 Promise 。
+ * @param {Object} config 用来定义 `leading` 和 `trailing` 行为的配置对象。 
+ * 默认为 `{ leading: true, trailing: false }` 。
+ * @return {Observable<T>} 该 Observable 执行节流操作，以限制源 Observable 的
+ * 发送频率。
  * @method throttle
  * @owner Observable
  */
