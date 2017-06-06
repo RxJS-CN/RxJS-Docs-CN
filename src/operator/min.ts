40b12bfe6f8b333ee40cf64b01f77845551a5a1a
SangKa.Z <sangka23@163.com>
2017-06-07 06:34:42 +0800

    docs(min): remove unnecessary original docs

--- src/operator/min.ts
+++ src/operator/min.ts
@@ -2,8 +2,6 @@ import { Observable } from '../Observable';
 import { ReduceOperator } from './reduce';
 
 /**
- * The Min operator operates on an Observable that emits numbers (or items that can be compared with a provided function),
- * and when source Observable completes it emits a single item: the item with the smallest value.
  * `min` 操作符操作的 Observable 发出数字(或可以使用提供函数进行比较的项)并且当源 Observable 完成时它发出单一项：最小值的项。
  *
  * <img src="./img/min.png" width="100%">
