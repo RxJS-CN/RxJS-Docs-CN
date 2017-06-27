437ce2e2558df32ff908baf88c9210129fdd53ad
SangKa.Z <sangka23@163.com>
2017-06-28 00:19:53 +0800

    chore(docs): update wechat share script

--- user/script/9-wechat-share.js
+++ user/script/9-wechat-share.js
@@ -4,7 +4,7 @@
 
     img.setAttribute('src', './manual/asset/Rx_Logo_M.png');
     img.setAttribute('class', 'wechat-share-log');
-    img.setAttribute('style', 'position:absolute;opacity:0;filter:alpha(opacity=0);');
+    img.setAttribute('style', 'position:absolute;left:-1000px;opacity:0;filter:alpha(opacity=0);');
     document.body.insertBefore(img, document.body.firstElementChild);
   });
 })();
