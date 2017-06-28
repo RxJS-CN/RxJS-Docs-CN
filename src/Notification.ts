3ab65f091dd6eb5de0ce63dd8681d3f995b01293
jianlei.hjl <jianlei.hjl@alibaba-inc.com>
2017-06-28 09:33:28 +0800

    docs(translate): src/Notification.ts

--- src/Notification.ts
+++ src/Notification.ts
@@ -2,12 +2,10 @@ import { PartialObserver } from './Observer';
 import { Observable } from './Observable';
 
 /**
- * Represents a push-based event or value that an {@link Observable} can emit.
- * This class is particularly useful for operators that manage notifications,
- * like {@link materialize}, {@link dematerialize}, {@link observeOn}, and
- * others. Besides wrapping the actual delivered value, it also annotates it
- * with metadata of, for instance, what type of push message it is (`next`,
- * `error`, or `complete`).
+ * 代表一个可以被 {@link Observable} 发出的基于推送的事件或者值。对于像 {@link materialize}，
+ *  {@link dematerialize}， {@link observeOn} 和其他管理通知的操作符来说，这类尤其适用。
+ * 除了包装真正发出的值，它还使用，例如，推送的类型（`next`，`error`， or `complete`）等元数据
+ * 装饰它。
  *
  * @see {@link materialize}
  * @see {@link dematerialize}
@@ -23,7 +21,7 @@ export class Notification<T> {
   }
 
   /**
-   * Delivers to the given `observer` the value wrapped by this Notification.
+   * 传递给给定的 `observer` 这个通知包装过的值。
    * @param {Observer} observer
    * @return
    */
@@ -39,11 +37,10 @@ export class Notification<T> {
   }
 
   /**
-   * Given some {@link Observer} callbacks, deliver the value represented by the
-   * current Notification to the correctly corresponding callback.
-   * @param {function(value: T): void} next An Observer `next` callback.
-   * @param {function(err: any): void} [error] An Observer `error` callback.
-   * @param {function(): void} [complete] An Observer `complete` callback.
+   * 给定一些 {@link Observer} 回调函数， 将当前通知所表示的值传递给正确的对应回调函数。
+   * @param {function(value: T): void} next Observer 的 `next` 回调函数。
+   * @param {function(err: any): void} [error] Observer 的 `error` 回调函数。
+   * @param {function(): void} [complete] An Observer 的 `complete` 回调函数。
    * @return {any}
    */
   do(next: (value: T) => void, error?: (err: any) => void, complete?: () => void): any {
@@ -59,12 +56,11 @@ export class Notification<T> {
   }
 
   /**
-   * Takes an Observer or its individual callback functions, and calls `observe`
-   * or `do` methods accordingly.
-   * @param {Observer|function(value: T): void} nextOrObserver An Observer or
-   * the `next` callback.
-   * @param {function(err: any): void} [error] An Observer `error` callback.
-   * @param {function(): void} [complete] An Observer `complete` callback.
+   * 接受一个 Observer 或者它的回调函数，然后相应的调用 `observe` 或者 `do` 方法。 
+   * @param {Observer|function(value: T): void} nextOrObserver  Observer 或者
+   * `next` 回调函数。
+   * @param {function(err: any): void} [error] Observer 的 `error` 回调函数。
+   * @param {function(): void} [complete] Observer 的 `complete` 回调函数。
    * @return {any}
    */
   accept(nextOrObserver: PartialObserver<T> | ((value: T) => void), error?: (err: any) => void, complete?: () => void) {
@@ -76,8 +72,7 @@ export class Notification<T> {
   }
 
   /**
-   * Returns a simple Observable that just delivers the notification represented
-   * by this Notification instance.
+   * 返回一个仅仅发出由该通知实例表示的通知的简单 Observable。
    * @return {any}
    */
   toObservable(): Observable<T> {
@@ -97,11 +92,9 @@ export class Notification<T> {
   private static undefinedValueNotification: Notification<any> = new Notification('N', undefined);
 
   /**
-   * A shortcut to create a Notification instance of the type `next` from a
-   * given value.
-   * @param {T} value The `next` value.
-   * @return {Notification<T>} The "next" Notification representing the
-   * argument.
+   * 使用给定的值创建一个`next`型通知实例的快捷方法。
+   * @param {T} value `next` 的值。
+   * @return {Notification<T>} 代表传入值的`next`型通知实例。
    */
   static createNext<T>(value: T): Notification<T> {
     if (typeof value !== 'undefined') {
@@ -111,19 +104,17 @@ export class Notification<T> {
   }
 
   /**
-   * A shortcut to create a Notification instance of the type `error` from a
-   * given error.
-   * @param {any} [err] The `error` error.
-   * @return {Notification<T>} The "error" Notification representing the
-   * argument.
+   * 使用给定的错误对象创建一个`error`型通知实例的快捷方法。
+   * @param {any} [err]  `error` 错误。
+   * @return {Notification<T>} 代表传入错误的`error`型通知实例。
    */
   static createError<T>(err?: any): Notification<T> {
     return new Notification('E', undefined, err);
   }
 
   /**
-   * A shortcut to create a Notification instance of the type `complete`.
-   * @return {Notification<any>} The valueless "complete" Notification.
+   * 创建一个`complete`型通知实例的快捷方法。
+   * @return {Notification<any>} 没有值的`complete`型的通知。
    */
   static createComplete(): Notification<any> {
     return this.completeNotification;
