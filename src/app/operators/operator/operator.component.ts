62cb886db95a6a2f8b1d64222f17deb2bd5873e1
btroncone <btroncone@gmail.com>
2017-10-04 11:30:07 -0400

    basic operator outline

---
+++ src/app/operators/operator/operator.component.ts
@@ -0,0 +1,8 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-operator',
+  templateUrl: './operator.component.html',
+  styleUrls: ['./operator.component.scss']
+})
+export class OperatorComponent {}
--- src/app/operators/operators.module.ts
+++ src/app/operators/operators.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MdSidenavModule, MdIconModule,  MdListModule } from '@angular/material';
+import { MdSidenavModule, MdIconModule,  MdListModule, MdToolbarModule } from '@angular/material';
 import { RouterModule } from '@angular/router';
 
 import { OperatorsComponent } from './operators.component';
+import { OperatorComponent } from './operator/operator.component';
 
 const OPERATOR_ROUTES = [
   {
@@ -14,14 +15,16 @@ const OPERATOR_ROUTES = [
 
 @NgModule({
   declarations: [
-    OperatorsComponent
+    OperatorsComponent,
+    OperatorComponent
   ],
   imports: [
     CommonModule,
     RouterModule.forChild(OPERATOR_ROUTES),
     MdSidenavModule,
     MdIconModule,
-    MdListModule
+    MdListModule,
+    MdToolbarModule
   ]
 })
 export class OperatorsModule { }
