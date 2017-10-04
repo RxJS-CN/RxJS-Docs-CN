05fc9341812e2776cbbc90bd6c0456d35637550f
btroncone <btroncone@gmail.com>
2017-10-04 10:59:38 -0400

    styling and grouping updates

--- src/app/operators/operators.component.ts
+++ src/app/operators/operators.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ALL_OPERATORS } from '../operator-docs';
+import { OperatorDoc } from '../operator-docs/operator.model';
 
 @Component({
   selector: 'app-operators',
@@ -7,11 +8,20 @@ import { ALL_OPERATORS } from '../operator-docs';
   styleUrls: ['./operators.component.scss']
 })
 export class OperatorsComponent implements OnInit {
-  public operators = ALL_OPERATORS.reduce(;
+  public operators = groupOperatorsByType(ALL_OPERATORS);
+  public categories = Object.keys(this.operators);
 
   constructor() { }
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
+}
+
+export function groupOperatorsByType(operators: OperatorDoc[]) {
+  return operators.reduce((acc, curr) => {
+    if (acc[curr.operatorType]) {
+      return { ...acc, [ curr.operatorType ] : [ ...acc[ curr.operatorType ], curr ] };
+    }
+    return { ...acc, [ curr.operatorType ]: [ curr ] };
+  }, {});
 }
---
+++ src/app/operators/specs/operators.spec.ts
@@ -0,0 +1,9 @@
+import { ALL_OPERATORS } from '../../operator-docs';
+import { groupOperatorsByType } from '../operators.component';
+
+describe('Operators', () => {
+  it('should group operators by operator type', () => {
+    const result = groupOperatorsByType(ALL_OPERATORS);
+    expect(Object.keys(result).length).toBe(8);
+  });
+});
