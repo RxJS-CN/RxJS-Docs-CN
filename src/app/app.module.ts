dc33074eed21f12a23e6c88968b198a7b2ee6c4c
Ashwin Sureshkumar <ashwin.sureshkumar@gmail.com>
2017-10-06 09:30:41 -0400

    Update angular material prefix to mat and lock down package versions
    - Updated imports and component prefixes for angular material
    - Updated package.json to lock down versions
    - Added .npmrc config to save-exact
    
    Close #25

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MdToolbarModule, MdSidenavModule, MdIconModule, MdButtonModule, MdListModule } from '@angular/material';
+import { MatToolbarModule, MatSidenavModule, MatIconModule, MatButtonModule, MatListModule } from '@angular/material';
 import { RouterModule } from '@angular/router';
 
 import { RXJS_DOC_ROUTES } from './app.routing';
@@ -24,11 +24,11 @@ import { RxjsComponent } from './rxjs/rxjs.component';
     BrowserModule,
     BrowserAnimationsModule,
     RouterModule.forRoot(RXJS_DOC_ROUTES),
-    MdToolbarModule,
-    MdSidenavModule,
-    MdIconModule,
-    MdButtonModule,
-    MdListModule
+    MatToolbarModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatButtonModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
