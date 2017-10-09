0595b30a362321d9af92b60e7e3d64c671494dd7
Andrew Anderson <Anderson7301@icloud.com>
2017-10-09 19:43:25 -0400

    consolidated router imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgModule } from '@angular/core';
 import { MatToolbarModule, MatSidenavModule, MatIconModule, MatButtonModule, MatListModule } from '@angular/material';
-import { RouterModule } from '@angular/router';
-import { PreloadAllModules } from '@angular/router';
-import { RXJS_DOC_ROUTES } from './app.routing';
+import { RouterModule, PreloadAllModules } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { RXJS_DOC_ROUTES } from './app.routing';
 
 @NgModule({
   declarations: [
