6653e668cef586a549708606b898695d2e699472
btroncone <btroncone@gmail.com>
2017-10-27 10:04:40 -0400

    fix(operators): added tips after operator description
    Added tips to display after operator long description if they exist. Also removed parameter section
    if no parameters exist.

--- src/app/operators/components/operator/operator.component.ts
+++ src/app/operators/components/operator/operator.component.ts
@@ -1,24 +1,29 @@
-import { Component, Input, OnInit, ChangeDetectionStrategy } from '@angular/core';
-import { OperatorDoc } from '../../../../operator-docs/operator.model';
+import {
+  Component,
+  Input,
+  OnInit,
+  ChangeDetectionStrategy
+} from "@angular/core";
+import { OperatorDoc } from "../../../../operator-docs/operator.model";
 
 @Component({
-  selector: 'app-operator',
-  templateUrl: './operator.component.html',
-  styleUrls: ['./operator.component.scss'],
+  selector: "app-operator",
+  templateUrl: "./operator.component.html",
+  styleUrls: ["./operator.component.scss"],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class OperatorComponent {
   @Input() operator: OperatorDoc;
 
-  private readonly baseSourceUrl = 'https://github.com/ReactiveX/rxjs/blob/master/src/operators/';
-  private readonly baseSpecUrl = 'http://reactivex.io/rxjs/test-file/spec-js/operators';
+  private readonly baseSourceUrl = "https://github.com/ReactiveX/rxjs/blob/master/src/operators/";
+  private readonly baseSpecUrl = "http://reactivex.io/rxjs/test-file/spec-js/operators";
 
   get operatorName() {
     return this.operator.name;
   }
 
   get signature() {
-    return this.operator.signature || 'Signature Placeholder';
+    return this.operator.signature || "Signature Placeholder";
   }
 
   get marbleUrl() {
@@ -30,15 +35,24 @@ export class OperatorComponent {
   }
 
   get shortDescription() {
-    return this.operator.shortDescription && this.operator.shortDescription.description;
+    return (
+      this.operator.shortDescription &&
+      this.operator.shortDescription.description
+    );
   }
 
   get shortDescriptionExtras() {
-    return this.operator.shortDescription && this.operator.shortDescription.extras;
+    return (
+      this.operator.shortDescription && this.operator.shortDescription.extras
+    );
   }
 
   get walkthrough() {
     return this.operator.walkthrough && this.operator.walkthrough.description;
+  }
+
+  get walkthroughExtras() {
+    return this.operator.walkthrough && this.operator.walkthrough.extras;
   }
 
   get parameters() {
