c66aadf2b577c7388b83dec65f7f5e4a16c24767
pmartin <contact@elol.fr>
2017-11-19 10:24:43 +0100

    refactor(operators): Separate operators in multiple pages

--- src/app/operators/operators-routing.module.ts
+++ src/app/operators/operators-routing.module.ts
@@ -2,8 +2,15 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { OperatorsComponent } from './operators.component';
+import { OperatorComponent } from './components/operator/operator.component';
 
-const routes: Routes = [{ path: '', component: OperatorsComponent }];
+const routes: Routes = [
+  {
+    path: '',
+    component: OperatorsComponent,
+    children: [{ path: ':operator', component: OperatorComponent }]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
