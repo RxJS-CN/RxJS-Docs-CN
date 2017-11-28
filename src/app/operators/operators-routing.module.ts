67bd96da0f4f86080059e30340ef929c8356af72
btroncone <btroncone@gmail.com>
2017-11-28 11:03:18 -0500

    feat(operators): default route to first operator when no name supplied

--- src/app/operators/operators-routing.module.ts
+++ src/app/operators/operators-routing.module.ts
@@ -3,13 +3,24 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { OperatorsComponent } from './operators.component';
 import { OperatorComponent } from './components/operator/operator.component';
+import { ALL_OPERATORS } from '../../operator-docs';
 
 const routes: Routes = [
   {
     path: '',
     component: OperatorsComponent,
     data: { title: ['Operators'], description: 'All the RxJS operators...' },
-    children: [{ path: ':operator', component: OperatorComponent }]
+    children: [
+      {
+        path: ':operator',
+        component: OperatorComponent
+      },
+      {
+        path: '',
+        redirectTo: ALL_OPERATORS[0].name,
+        pathMatch: 'full'
+      }
+    ]
   }
 ];
 
